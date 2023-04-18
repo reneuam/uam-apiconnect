e586082330b90081b4df791784d36125a6f43cf6
reneuam <renet@uassistme.com>
2023-04-18 15:34:10 -0600

    Add hubspot module and htt module to use axcios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HubspotModule } from './hubspot/hubspot.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), 
+    HttpModule,
+    HubspotModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
