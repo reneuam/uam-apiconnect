6e1486c866c6fe3aca0092e695cc5e0b12192efd
reneuam <renet@uassistme.com>
2023-04-26 12:44:09 -0600

    Add TypeORM deal repository to deal module

--- src/api/hubspot/deal/deal.module.ts
+++ src/api/hubspot/deal/deal.module.ts
@@ -1,11 +1,17 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DealService } from './deal.service';
 import { DealController } from './deal.controller';
 import { HubspotModule } from '../hubspot.module';
 import { ApiHubspotModule } from 'src/common/services/hubspot/api-hubspot.module';
+import { Deal } from './entities/deal.entity';
 
 @Module({
-  imports: [ApiHubspotModule, forwardRef(() => HubspotModule)],
+  imports: [
+    TypeOrmModule.forFeature([Deal]),
+    ApiHubspotModule,
+    forwardRef(() => HubspotModule)
+  ],
   controllers: [DealController],
   providers: [DealService]
 })
