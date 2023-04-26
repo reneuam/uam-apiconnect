7c5126dc065c298ce7277a10597e4c7b707fa155
reneuam <renet@uassistme.com>
2023-04-26 11:41:27 -0600

    Add bulkCreate and migrate function to deal service

--- src/api/hubspot/deal/deal.service.ts
+++ src/api/hubspot/deal/deal.service.ts
@@ -13,6 +13,27 @@ export class DealService {
     return 'This action adds a new deal';
   }
 
+  async bulkCreate(deals: any) {
+    const createdDeals = await Promise.all(
+      deals.map((deal: any) => console.log())
+    );
+  }
+
+  async migrate() {
+    let findParams = { after: undefined };
+    let allowToContinue = true;
+
+    while (allowToContinue) {
+      const deals = await this.dealHubspotApiService.findAll(findParams);
+
+      (deals.paging?.next.after) ?
+        findParams.after = deals.paging.next.after :
+        allowToContinue = false;
+
+      await this.bulkCreate(deals.results);
+    }
+  }
+
   async findAll(findParams: any) {
     return await this.dealHubspotApiService.findAll(findParams);
   }
