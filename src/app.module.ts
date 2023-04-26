c7a7182a5ce49147f65782d2cc93d5a7f1d53ec7
reneuam <renet@uassistme.com>
2023-04-26 11:51:30 -0600

    Add TypeOrmModule configuration to use my hubspot database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,18 @@ import { ApiHubspotModule } from './common/services/hubspot/api-hubspot.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as any,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username  : process.env.DATABASE_USER,
+      password  : process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+      extra : {
+        connectionLimit: 100
+      }
+    }),
     ApiHubspotModule,
     HttpModule,
     HubspotModule
