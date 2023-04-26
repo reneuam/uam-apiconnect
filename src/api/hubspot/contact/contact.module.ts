cdff5506635b9bdb98f535109cba06d923074f31
reneuam <renet@uassistme.com>
2023-04-26 12:42:48 -0600

    Add TypeORM contact repository to contact module

--- src/api/hubspot/contact/contact.module.ts
+++ src/api/hubspot/contact/contact.module.ts
@@ -1,11 +1,17 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
 import { HubspotModule } from '../hubspot.module';
 import { ApiHubspotModule } from 'src/common/services/hubspot/api-hubspot.module';
+import { Contact } from './entities/contact.entity';
 
 @Module({
-  imports: [ApiHubspotModule, forwardRef(() => HubspotModule)],
+  imports: [
+    TypeOrmModule.forFeature([Contact]),
+    ApiHubspotModule,
+    forwardRef(() => HubspotModule)
+  ],
   controllers: [ContactController],
   providers: [ContactService]
 })
