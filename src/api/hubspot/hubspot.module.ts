42ee9c13daba9a62f1d958dfb1880d8054b23545
reneuam <renet@uassistme.com>
2023-05-02 10:56:29 -0600

    Add HubspotController to HubspotModule

--- src/api/hubspot/hubspot.module.ts
+++ src/api/hubspot/hubspot.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { HubspotService } from './hubspot.service';
 import { ContactModule } from './contact/contact.module';
 import { DealModule } from './deal/deal.module';
+import { HubspotController } from './hubspot.controller';
 
 @Module({
   imports: [ContactModule, DealModule],
   providers: [HubspotService],
-  exports: [HubspotService]
+  exports: [HubspotService],
+  controllers: [HubspotController]
 })
 export class HubspotModule {}
