5e16afe63f32f6972a694d25a3e6f2c2ad06f9e4
reneuam <renet@uassistme.com>
2023-05-02 10:51:23 -0600

    Add subscription type constants

---
+++ src/common/constants/hubspot-subscription-type.ts
@@ -0,0 +1,8 @@
+export class HubspotSubscriptionType {
+  static readonly contactCreated = 'contact.creation';
+  static readonly contactUpdated = 'contact.propertyChange';
+  static readonly contactDeleted = 'contact.deletion';
+  static readonly dealCreated = 'deal.creation';
+  static readonly dealUpdated = 'deal.propertyChange';
+  static readonly dealDeleted = 'deal.deletion';
+}
