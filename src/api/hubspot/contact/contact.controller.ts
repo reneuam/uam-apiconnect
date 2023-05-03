a43a89bb801d1437d213f2b978228ce6d584d9a8
reneuam <renet@uassistme.com>
2023-05-03 09:55:02 -0600

    Remove create, update, remove and set migration on findAll function for contact controller

--- src/api/hubspot/contact/contact.controller.ts
+++ src/api/hubspot/contact/contact.controller.ts
@@ -1,37 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ContactService } from './contact.service';
-import { CreateContactDto } from './dto/create-contact.dto';
-import { UpdateContactDto } from './dto/update-contact.dto';
 
 @Controller({ path: 'hubspot/contacts', version: '1' })
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
-
-  @Post()
-  create(@Body() createContactDto: CreateContactDto) {
-    return this.contactService.create(createContactDto);
-  }
 
   @Get()
   findAll(@Query() queryParams) {
     const after = (queryParams.after) ? queryParams.after : undefined;
     const findParams = { after };
 
-    return this.contactService.findAll(findParams);
+    return this.contactService.migrate();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.contactService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
-    return this.contactService.update(id, updateContactDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.contactService.remove(id);
   }
 }
