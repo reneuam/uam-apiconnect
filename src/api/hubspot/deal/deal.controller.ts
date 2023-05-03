9e25746c91b9c815fb15f5f14f877e682951e877
reneuam <renet@uassistme.com>
2023-05-03 09:56:52 -0600

    Remove create, update, remove and set migration on findAll function for deal controller

--- src/api/hubspot/deal/deal.controller.ts
+++ src/api/hubspot/deal/deal.controller.ts
@@ -1,37 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { DealService } from './deal.service';
-import { CreateDealDto } from './dto/create-deal.dto';
-import { UpdateDealDto } from './dto/update-deal.dto';
 
 @Controller({ path: 'hubspot/deals', version: '1' })
 export class DealController {
   constructor(private readonly dealService: DealService) {}
-
-  @Post()
-  create(@Body() createDealDto: CreateDealDto) {
-    return this.dealService.create(createDealDto);
-  }
 
   @Get()
   findAll(@Query() queryParams) {
     const after = (queryParams.after) ? queryParams.after : undefined;
     const findParams = { after };
 
-    return this.dealService.findAll(findParams);
+    return this.dealService.migrate();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.dealService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDealDto: UpdateDealDto) {
-    return this.dealService.update(id, updateDealDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.dealService.remove(id);
   }
 }
