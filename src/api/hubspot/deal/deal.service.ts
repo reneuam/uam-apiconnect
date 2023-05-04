f187b7f3b59b415d1b0b07167fc5ee311aa97629
reneuam <renet@uassistme.com>
2023-05-04 11:48:07 -0600

    Add CRUD functions on deal service

--- src/api/hubspot/deal/deal.service.ts
+++ src/api/hubspot/deal/deal.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateDealDto } from './dto/create-deal.dto';
-import { UpdateDealDto } from './dto/update-deal.dto';
 import { DealHubspotApiService } from 'src/common/services/hubspot/deal-hubspot.service';
 import { Deal } from './entities/deal.entity';
+import { parseToDouble, parseToInt } from 'src/common/utils/number-parser';
+import { DealProperties } from 'src/common/constants/hubspot-deal-properties';
+import { boolStringToInt } from 'src/common/utils/bool-string-parser';
 
 @Injectable()
 export class DealService {
@@ -14,13 +15,92 @@ export class DealService {
     private readonly dealRepository: Repository<Deal>
   ) {}
 
-  create(createDealDto: CreateDealDto) {
-    return 'This action adds a new deal';
+  async create(id: string) {
+    console.log('Create deal id', id);
+    const deal = await this.dealHubspotApiService.findOne(id);
+
+    if (deal) {
+      const agent_acount = parseToInt(deal.properties.agent_acount);
+      const amount = parseToDouble(deal.properties.amount);
+      const amount_in_home_currency = parseToDouble(deal.properties.amount_in_home_currency);
+      const contract_value = parseToDouble(deal.properties.contract_value);
+      const headcount_needed = parseToInt(deal.properties.headcount_needed);        
+      const hs_is_closed_won = boolStringToInt(deal.properties.hs_is_closed_won);
+      const paid = boolStringToInt(deal.properties.paid);
+      const price_per_hour = parseToDouble(deal.properties.price_per_hour);
+      const reactivation = boolStringToInt(deal.properties.reactivation);
+      const sales_cycle = parseToInt(deal.properties.sales_cycle);
+
+      const dealObj = {
+        id: deal.id,
+        ...deal.properties,
+        agent_acount,
+        amount,
+        amount_in_home_currency,
+        contract_value,
+        headcount_needed,
+        hs_is_closed_won,
+        paid,
+        price_per_hour,
+        reactivation,
+        sales_cycle
+      };
+      
+      const properties = DealProperties;
+
+      const dealFilteredObj = Object.keys(dealObj)
+        .filter(key => properties.includes(key))
+        .reduce((newObj, key) => {
+          newObj[key] = dealObj[key];
+          return newObj;
+        }, {});
+
+      return await this.dealRepository.save(dealFilteredObj);
+    }
+
+    return null;
   }
 
   async bulkCreate(deals: any) {
-    const createdDeals = await Promise.all(
-      deals.map((deal: any) => console.log())
+    return await Promise.all(
+      deals.map((deal: any) => {        
+        const agent_acount = parseToInt(deal.properties.agent_acount);
+        const amount = parseToDouble(deal.properties.amount);
+        const amount_in_home_currency = parseToDouble(deal.properties.amount_in_home_currency);
+        const contract_value = parseToDouble(deal.properties.contract_value);
+        const headcount_needed = parseToInt(deal.properties.headcount_needed);        
+        const hs_is_closed_won = boolStringToInt(deal.properties.hs_is_closed_won);
+        const paid = boolStringToInt(deal.properties.paid);
+        const price_per_hour = parseToDouble(deal.properties.price_per_hour);
+        const reactivation = boolStringToInt(deal.properties.reactivation);
+        const sales_cycle = parseToInt(deal.properties.sales_cycle);
+
+        const dealObj = {
+          id: deal.id,
+          ...deal.properties,
+          agent_acount,
+          amount,
+          amount_in_home_currency,
+          contract_value,
+          headcount_needed,
+          hs_is_closed_won,
+          paid,
+          price_per_hour,
+          reactivation,
+          sales_cycle
+        };
+
+        const properties = DealProperties;
+
+        const dealFilteredObj = Object.keys(dealObj)
+          .filter(key => properties.includes(key))
+          .reduce((newObj, key) => {
+            newObj[key] = dealObj[key];
+            return newObj;
+          }, {});
+
+        this.dealRepository.save(dealFilteredObj);
+      })
     );
   }
 
@@ -39,19 +119,69 @@ export class DealService {
     }
   }
 
-  async findAll(findParams: any) {
-    return await this.dealHubspotApiService.findAll(findParams);
+  async findAll() {
+    return await this.dealRepository.find();
   }
 
   async findOne(id: string) {
-    return await this.dealHubspotApiService.findOne(id);
+    return await this.dealRepository.findOneBy({ id });
   }
 
-  update(id: string, updateDealDto: UpdateDealDto) {
-    return `This action updates a #${id} deal`;
+  async update(id: string) {
+    console.log('update deal id', id);
+    const deal = await this.dealHubspotApiService.findOne(id);
+
+    if (deal) {
+      const agent_acount = parseToInt(deal.properties.agent_acount);
+      const amount = parseToDouble(deal.properties.amount);
+      const amount_in_home_currency = parseToDouble(deal.properties.amount_in_home_currency);
+      const contract_value = parseToDouble(deal.properties.contract_value);
+      const headcount_needed = parseToInt(deal.properties.headcount_needed);        
+      const hs_is_closed_won = boolStringToInt(deal.properties.hs_is_closed_won);
+      const paid = boolStringToInt(deal.properties.paid);
+      const price_per_hour = parseToDouble(deal.properties.price_per_hour);
+      const reactivation = boolStringToInt(deal.properties.reactivation);
+      const sales_cycle = parseToInt(deal.properties.sales_cycle);
+
+      const dealObj = {
+        id: deal.id,
+        ...deal.properties,
+        agent_acount,
+        amount,
+        amount_in_home_currency,
+        contract_value,
+        headcount_needed,
+        hs_is_closed_won,
+        paid,
+        price_per_hour,
+        reactivation,
+        sales_cycle
+      };
+
+      const properties = DealProperties;
+
+      const dealFilteredObj = Object.keys(dealObj)
+        .filter(key => properties.includes(key))
+        .reduce((newObj, key) => {
+          newObj[key] = dealObj[key];
+          return newObj;
+        }, {});
+
+      return await this.dealRepository.update(id, dealFilteredObj);
+    }
+
+    return null;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} deal`;
+  async remove(id: string) {
+    console.log('Remove deal id', id);
+    
+    const deal = await this.findOne(id);
+
+    if (deal) {
+      return await this.dealRepository.delete(id);
+    }
+
+    return null;
   }
 }
