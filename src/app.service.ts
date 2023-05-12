3a7c7b949252e881e0022786f3eac73b6150c2a0
reneuam <renet@uassistme.com>
2023-05-12 10:13:32 -0600

    Add ticket service migrate function to onApplicationBootstrap on  app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,20 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { ContactService } from './api/hubspot/contact/contact.service';
 import { DealService } from './api/hubspot/deal/deal.service';
+import { TicketService } from './api/hubspot/ticket/ticket.service';
 
 @Injectable()
 export class AppService implements OnApplicationBootstrap {
   constructor(
     private readonly contactService: ContactService,
-    private readonly dealService: DealService
+    private readonly dealService: DealService,
+    private readonly ticketService: TicketService
   ) {}
 
   async onApplicationBootstrap() {
     const contacts = await this.contactService.findAll();
     const deals = await this.dealService.findAll();
+    const tickets = await this.ticketService.findAll();
     
     if (contacts.length === 0) {
       await this.contactService.migrate();
@@ -19,6 +22,10 @@ export class AppService implements OnApplicationBootstrap {
 
     if (deals.length === 0) {
       await this.dealService.migrate();
+    }
+
+    if (tickets.length === 0) {
+      await this.ticketService.migrate();
     }
   }
 }
